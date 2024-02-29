01188a431cd4e8d36019237d0c508665e0882bbc
Jørgen Gundersen <e773438@tietoevry-owned-address-139-107-200-x.hidden-host.tietoevry.com>
2024-02-29 08:31:38 +0100

    feat: Custom command for cleaning up maven install log for spring boot

--- lua/jg/custom_commands.lua
+++ lua/jg/custom_commands.lua
@@ -1,3 +1,24 @@
 vim.api.nvim_create_user_command("FullPath", function()
 	print(vim.fn.expand('%:p'))
 end, {})
+
+local function deleteLinesAbovePattern(pattern)
+	local lineNum = vim.fn.search(pattern, "nW")
+
+	if lineNum > 1 then
+		vim.cmd('1,' .. lineNum .. 'd')
+	end
+end
+
+vim.api.nvim_create_user_command(
+	"ParseSpringBootLog",
+	function()
+		-- Remove all lines above pattern :: Spring Boot ::
+		deleteLinesAbovePattern(":: Spring Boot ::")
+
+		-- Remove all lines that do not start with "at"
+		vim.cmd('g/^\tat/d')
+
+		-- Remove all lines that do not start with "Caused by:"
+		vim.cmd('g/Caused by:/normal O')
+	end, {})
